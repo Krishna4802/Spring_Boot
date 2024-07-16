f1ab318b9a8f8714d8284ae1f5669195e3151397
Krish <krishnahashagile@gmail.com>
2024-07-16 17:54:31 +0530

    Chat Application
    Chat Application development with Mongodb

---
+++ Chat_app/src/main/java/com/krish/Websocket/user/Status.java
@@ -0,0 +1,6 @@
+package com.krish.Websocket.user;
+
+public enum Status {
+
+    ONLINE,OFFLINE
+}
---
+++ Chat_app/src/main/java/com/krish/Websocket/user/UserRepository.java
@@ -0,0 +1,9 @@
+package com.krish.Websocket.user;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    List<User> findAllByStatus(Status status);
+}
